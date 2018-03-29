b1cdec8c3d7954785875d5246c8daf4a8a8fae2f
Faizanshah007 <faizanshah.ansari@gmail.com>
2018-03-30 00:42:35 +0530

    Add files via upload

---
+++ anagram_game.py
@@ -0,0 +1,11 @@
+from pygame import *
+
+fps = 60
+
+
+
+mainClock = pygame.time.Clock()
+
+pygame.display.set_caption("Anaglink")
+Canvas = pygame.display.set_mode((window_width,window_height))#,pygame.FULLSCREEN)
+
